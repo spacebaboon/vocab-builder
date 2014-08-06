937455d712bad0727c62d2cb40aba551cde5430e
Ben Avery <spacebaboon@gmail.com>
2014-08-06 01:40:18 +0100

    split main content out into separate views, and add routing, to allow the vocab revision page.

--- public/core.js
+++ public/core.js
@@ -1,43 +1,61 @@
 // public/core.js
-var vocabApp = angular.module('vocabApp', []);
+angular.module('vocabApp', ['ngRoute']).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/vocab.html',
+                controller: 'VocabController'
+            })
+            .when('/revise', {
+                templateUrl: 'views/revise.html',
+                controller: 'ReviseController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
 
-function mainController($scope, $http) {
-	$scope.formData = {};
+function VocabController($scope, $http) {
+    $scope.formData = {};
 
-	// when landing on the page, get all words and show them
-	$http.get('/api/words')
-		.success(function(data) {
-			$scope.words = data;
-			console.log(data);
-		})
-		.error(function(data) {
-			console.log('Error: ' + data);
-		});
+    // when landing on the page, get all words and show them
+    $http.get('/api/words')
+        .success(function (data) {
+            $scope.words = data;
+            console.log(data);
+        })
+        .error(function (data) {
+            console.log('Error: ' + data);
+        });
 
-	// when submitting the add form, send the text to the node API
-	$scope.createWord = function() {
-		$http.post('/api/words', $scope.formData)
-			.success(function(data) {
-				$scope.formData = {}; // clear the form so our user is ready to enter another
-				$scope.words = data;
-				console.log(data);
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
-	};
+    // when submitting the add form, send the text to the node API
+    $scope.createWord = function () {
+        $http.post('/api/words', $scope.formData)
+            .success(function (data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.words = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
 
-	// delete a word after checking it
-	$scope.deleteWord = function(id) {
-		$http.delete('/api/words/' + id)
-			.success(function(data) {
-				$scope.words = data;
-				console.log(data);
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
-	};
+    // delete a word after checking it
+    $scope.deleteWord = function (id) {
+        $http.delete('/api/words/' + id)
+            .success(function (data) {
+                $scope.words = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
 
 }
+
+function ReviseController($scope, $http) {
+    $scope.message = 'Hello Ben!';
+};
 
