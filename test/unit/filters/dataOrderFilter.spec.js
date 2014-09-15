64bed94f701083ad93ea49d6ecba742d8ad76ac5
Ben Avery <spacebaboon@gmail.com>
2014-09-15 20:48:42 +0100

    add data dump dir and initial dump. remove unused code.

--- test/unit/filters/dataOrderFilter.spec.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-describe('Filter: dataOrderFilter', function() {
-
-    it('should reverse a list', function () {
-        var list = [{"id": "1", "val": "one"},{"id": "2", "val": "two"},{"id": "3", "val": "three"} ];
-    })
-})
