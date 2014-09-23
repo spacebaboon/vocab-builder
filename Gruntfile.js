092bbe1d13713d7120792bc4ab3894a46e178fbc
Ben Avery <spacebaboon@gmail.com>
2014-09-23 23:21:02 +0100

    add Grunt to start server and to run Karma and Protractor tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,46 @@
 
 module.exports = function(grunt) {
 
-
-//    require('load-grunt-tasks')(grunt);
-
     grunt.initConfig({
+
+        // configure nodemon
+        nodemon: {
+            dev: {
+                script: 'server.js'
+            }
+        },
 
         karma: {
             unit: {
                 configFile: 'test/karma.conf.js',
                 singleRun: true
             }
-        }
+        },
+
+        protractor: {
+            options: {
+                configFile: "node_modules/protractor/referenceConf.js", // Default config file
+                keepAlive: true, // If false, the grunt process stops when the test fails.
+                noColor: false, // If true, protractor will not use colors in its output.
+                args: {
+                    // Arguments passed to the command
+                }
+            },
+            protractor: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too.
+                options: {
+                    configFile: "protractor_conf.js", // Target-specific config file
+                    args: {} // Target-specific arguments
+                }
+            },
+        },
 
     });
 
-    grunt.registerTask('test', [
-       'karma'
-    ]);
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-protractor-runner');
 
-    grunt.registerTask('run', [
+    grunt.registerTask('test', ['karma', 'protractor']);
+    grunt.registerTask('default', ['nodemon']);
 
-    ])
-}
+};
