87057001fea915c24c987909efdac06443c76f8c
Ben Avery <spacebaboon@gmail.com>
2014-11-12 01:22:49 +0000

    add sass to gruntfile, install sass, neat, bourbon.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,54 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+
+        sass: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/scss',
+                    src: 'all.scss',
+                    dest: 'public/css/',
+                    ext: '.css'
+                }],
+
+                options: {
+                    bundleExec: false, // runs the sass with "bundle exec sass" using Bundler (http://bundler.io/)
+                    loadPath: ['public/scss', 'public/bower_components'],
+                    style: 'compressed',
+                    trace: true,
+                    require: ['bourbon', 'neat']
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['public/scss/**/*.scss'],
+                tasks: ['sass', 'csslint', 'legacssy:ie8']
+            }
+//            js: {
+//                files: ['public/js/**/*.js'],
+//                tasks: ['jshint']
+//            }
+        },
+
+        jshint: {
+            files: ['newShopStatic/_assets/js/modules/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        csslint: {
+            base: {
+                src: ['newShopStatic/_assets/css/**/*.css']
+            },
+            options: {
+                csslintrc: '.csslintrc'
+            }
+        },
+
         nodemon: {
             dev: {
                 script: 'server.js'
@@ -46,8 +94,9 @@ module.exports = function(grunt) {
             options: {
                 logConcurrentOutput: true,
             },
-            tasks: ['nodemon', 'karma', 'protractor', 'nodemon:dev:stop']
+            tasks: ['watch', 'nodemon']
         }
+
 
     });
 
@@ -56,8 +105,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-protractor-runner');
     grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('build', ['sass', 'karma']);
 
     grunt.registerTask('test', ['express:dev', 'karma', 'protractor']);
+
+    grunt.registerTask('default', ['development', 'concurrent']);
+
     grunt.registerTask('default', ['nodemon']);
 
 };
